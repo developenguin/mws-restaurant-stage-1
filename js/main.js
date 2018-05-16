b109fbc8b08c4352f4f8defe53b4382a8019907f
matthijs <matthijs@heavymetalpenguin.com>
2018-05-16 21:22:02 +0200

    Add a few aria-label attributes to make viewing details easier and fix the API key for restaurant details

--- js/main.js
+++ js/main.js
@@ -159,10 +159,12 @@ createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
+  more.setAttribute('aria-label', `view details for ${restaurant.name}`);
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more);
 
-  return li
+  return li;
+
 };
 
 /**
